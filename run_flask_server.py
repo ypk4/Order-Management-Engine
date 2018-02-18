cf1544b6b097baa459dcf284a5dae82cc437e91b
Chromares <shantanuchandorkar@gmail.com>
2018-02-18 13:09:12 +0530

    MongoBD is up, added code to connect DB

--- run_flask_server.py
+++ run_flask_server.py
@@ -1,55 +1,32 @@
-# USAGE
-# Start the server :
-# 	python run_keras_server.py
-# Submit a request via cURL from different terminal :
-# 	curl  -X POST  -H "Content-Type: application/json"  -d '{"key1":"value1", "key2":"value2"}' 'http://localhost:5000/order_endpoint'   OR
-# Submit a request via separate Python / PHP / other language code from different terminal :
-#	python order_request.py
-#	python execution_request.py	
-
-
-# import the necessary packages
 import flask
-
-
-# initialize our Flask application and the Keras model
+from flask_pymongo import PyMongo
 app = flask.Flask(__name__)
-
-
+app.config['MONGO_DBNAME'] = 'order_entry'
+app.config['MONGO_URI'] = 'mongodb://admin:thisisyouradmin@ds141068.mlab.com:41068/order_entry'
 # Following endpoint corresponds to order_entry() function defined below it. It can be called by client on 'http://localhost:5000/order_endpoint' url.
+mongo = PyMongo(app)
 
 @app.route("/order_endpoint", methods=["POST"])
 def order_entry():
 	# initialize the ack dictionary that will be returned from the view
+	order = mongo.db.orders
 	ack = {"success": False}
-
 	if flask.request.method == "POST":
 		#print request.data
 		if flask.request.is_json:
 			content = flask.request.get_json()	
-			print content
-		
-			print 'IP of sender : ', flask.request.remote_addr
+			#print content
+			#print 'IP of sender : ', flask.request.remote_addr
 			#print flask.request.environ['REMOTE_ADDR']
-						
-			# Our logic/code for the received JSON :-
-			
-			# e.g.- Adding order received from order entry
-			
 			order_id = content['order_id']
 			user_id = content['user_id']
 			side = content['side']
 			ask_price = content['ask_price']
-			
-			# Write Flask-MongoEngine code to insert above retrieved values OR if we want to directly store JSON, then directly store
-			#
-
-			# indicate that the request was a success
+			order.insert({'oder_id' : order_id, 'user_id' : user_id, 'side' : side, ask_price : 'ask_price'})
 			ack["success"] = True
 
 	# return the data dictionary as a JSON response
 	return flask.jsonify(ack)
-
 
 # Following endpoint corresponds to execution_links() function defined below it. It can be called by client on 'http://localhost:5000/execution_endpoint' url.
 
@@ -57,43 +34,26 @@ def order_entry():
 def execution_links():
 	# initialize the ack dictionary that will be returned from the view
 	ack = {"success": False}
-
+	fill = mongo.db.fills
 	if flask.request.method == "POST":
 		#print request.data
 		if flask.request.is_json:
 			content = flask.request.get_json()	
 			print content
-
 			#print flask.request.environ['REMOTE_ADDR']
-			print 'IP of sender : ', flask.request.remote_addr
-			
-			# Our logic/code for the received JSON :-
-			
-			# e.g.- Storing fill coming from execution links, etc
-			
+			#print 'IP of sender : ', flask.request.remote_addr
 			fill_ids = content['fill_ids']
 			order_id = content['order_id']
 			qtydone = content['qtydone']
 			prices = content['prices']
 			exchange_id = content['exchange_id']
-			
-			# Write Flask-MongoEngine code for insertion into db
-			#
-
+			fill.insert({'fill_ids' : fill_ids, 'order_id' : order_id, 'qtydone' : qtydone, 'prices' : prices, 'exchange_id' : exchange_id})
 			# indicate that the request was a success
 			ack["success"] = True
-
-	# return the data dictionary as a JSON response
 	return flask.jsonify(ack)
-
 
 # if this is the main thread of execution first load the model and then start the server
 if __name__ == "__main__":
 	print(("* Flask starting server..."
 		"Please wait until server has fully started"))
-
-	# Any code that we want to run only once at the start at server side :-
-	# e.g. - Loading trained classification model should be done only once at start of server and not for each POST request
-
-
-	app.run()
+	app.run(debug=True)
